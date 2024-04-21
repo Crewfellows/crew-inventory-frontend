fc661b2f0f40a674b89ec351e0b235c0a890a84f
maazu <maaz581@gmail.com>
2024-04-21 19:18:35 +0100

    style: project configuration setup

---
+++ src/app/(routes)/login/page.tsx
@@ -0,0 +1,3 @@
+export default function Login() {
+  return <p>THIS IS A Login Page</p>;
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
 import { Metadata } from 'next';
 
-import './globals.css';
+import '@/styles/globals.css';
 
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: `TypeScript starter for Next.js by João Pedro Schmitz`,
-  description: `TypeScript starter for Next.js that includes all you need to build amazing apps`,
+  title: `CrewInventory | Crewfellows Limited |`,
+  description: ``,
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
