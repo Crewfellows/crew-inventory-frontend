47a98dfd6204402b6f0413b4044914f1eb77f78f
Abdul <sami.sayani10@gmail.com>
2024-04-28 22:54:43 +0500

    refactor: layout and login

--- src/app/(routes)/(dashboard)/layout.tsx
+++ src/app/(routes)/(dashboard)/layout.tsx
@@ -1,3 +1,49 @@
+'use client';
+
+import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
+import { Button, Layout, Menu, theme } from 'antd';
+import React, { useState } from 'react';
+
+import { sideBarNavigation } from '@/app/config/navigation';
+
+const { Header, Sider, Content } = Layout;
+
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  return (
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} items={sideBarNavigation} />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 'auto',
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          {children}
+        </Content>
+      </Layout>
+    </Layout>
+  );
 }
---
+++ src/app/config/navigation.tsx
@@ -0,0 +1,19 @@
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+
+export const sideBarNavigation = [
+  {
+    key: '1',
+    icon: <UserOutlined />,
+    label: 'Inventory Managment',
+  },
+  {
+    key: '2',
+    icon: <VideoCameraOutlined />,
+    label: 'Sale Managment',
+  },
+  {
+    key: '3',
+    icon: <UploadOutlined />,
+    label: 'Stock Report',
+  },
+];
