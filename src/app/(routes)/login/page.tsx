895a4782c57d6659cc2944cb18b895cd5c89abfd
maazu <maaz581@gmail.com>
2024-05-12 21:25:05 +0100

    refactor: login form page update

--- src/app/(routes)/login/page.tsx
+++ src/app/(routes)/login/page.tsx
@@ -1,62 +1,14 @@
 'use client';
 
-import { Button, Checkbox, Col, Form, Input, Row } from 'antd';
+import { Row } from 'antd';
 import React from 'react';
 
-import type { FormProps } from 'antd';
+import LoginForm from '@/components/LoginForm';
 
-type FieldType = {
-  username?: string;
-  password?: string;
-  remember?: string;
-};
-
-const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
-  console.log('Success:', values);
-};
-
-const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
-  console.log('Failed:', errorInfo);
-};
 export default function Login() {
   return (
-    <Row justify={'center'} align={'middle'}>
-      <Col span={24}>
-        <Form
-          name="basic"
-          style={{ maxWidth: 600, margin: 'auto' }}
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
-        >
-          <Form.Item<FieldType>
-            label="Email"
-            name="email"
-            rules={[{ required: true, message: 'Please input your username!' }]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item<FieldType>
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: 'Please input your password!' }]}
-          >
-            <Input.Password />
-          </Form.Item>
-
-          <Form.Item<FieldType> name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-            <Checkbox>Remember me</Checkbox>
-          </Form.Item>
-
-          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-      </Col>
+    <Row>
+      <LoginForm />
     </Row>
   );
 }
